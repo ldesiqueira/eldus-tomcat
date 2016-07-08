03e92d8fad84aff64fb2a9f17da83305cc356e56
ldesiqueira <eldusfelipus@gmail.com>
2016-07-08 11:53:45 -0700

    init

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'louthebrew@gmail.com'
 license 'all_rights'
 description 'Installs/Configures tomcat for great justice'
 long_description 'Installs/Configures tomcat for great justice'
-version '0.1.0'
+version '0.1.1'
 
 depends 'poise'
